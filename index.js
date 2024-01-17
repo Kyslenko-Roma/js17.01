f9cb845d69d8a104ec1c280e2d73684ab3769129
Kyslenko-Roma <112819019+Kyslenko-Roma@users.noreply.github.com>
2024-01-17 07:54:09 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const yourName = prompt("Введіть імя");
+const yourAge = prompt("Введіть вік")
+const sumNumber1 = +prompt("Введіть число");
+const sumNumber2 = +prompt("Введіть число");
+
+alert("привіт " + yourName);
+alert("тобі " + yourAge);
+
+alert (sumNumber1 + sumNumber2);
